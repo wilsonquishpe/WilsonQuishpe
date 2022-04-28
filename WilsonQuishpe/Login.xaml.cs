8aa459c0e614741f17583def77be68aaae3d86a5
Wilson Quishpe <Wilson Quishpe@DESKTOP-CV9L9KS>
2022-04-27 22:00:45 -0500

    Agregar archivos de proyecto.

---
+++ WilsonQuishpe/Login.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WilsonQuishpe
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnAbrir_Clicked(object sender, EventArgs e)
+        {
+            string estudiante = txtUsuario.Text;
+            string contrasena = txtPass.Text;
+            if (estudiante == "wilson2022" && contrasena == "uisrael2022")
+            {
+                await Navigation.PushAsync(new Registro(estudiante));
+            }
+            else
+            {
+                await DisplayAlert("Atención", "Datos Incorrectos", "OK");
+            }
+        }
+    }
+}
---
+++ WilsonQuishpe/Registro.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WilsonQuishpe
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Registro : ContentPage
+    {
+        public Registro(string estudiante)
+        {
+            InitializeComponent();
+            lblEstudiante.Text = "Estudiante Conectado: " + estudiante;
+        }
+
+        private void btnCalcularPM_Clicked(object sender, EventArgs e)
+        {
+
+            int valor = 3000;
+
+            double pMensual = (valor - double.Parse(txtCInicial.Text)) / 5;
+
+            double pagoMensualTotal = (pMensual * 0.05) + pMensual;
+
+            if (double.Parse(txtCInicial.Text) < 3000)
+            {
+
+                txtPago.Text = pagoMensualTotal.ToString();
+            }
+            else
+            {
+                DisplayAlert("Atención", "El valor no debe sobrepasar 3000", "OK");
+            }
+        }
+
+        private async void btnSave_Clicked(object sender, EventArgs e)
+        {
+          
+            if (double.Parse(txtCInicial.Text) < 3000)
+            {
+                await DisplayAlert("Atención", "Registro guardado con éxtio", "OK");
+                double total = double.Parse(txtCInicial.Text) + (double.Parse(txtPago.Text) * 5);
+
+                await Navigation.PushAsync(new Resumen(lblEstudiante.Text, txtNombre.Text, total));
+            }
+            else
+            {
+               await DisplayAlert("Atención", "El valor no debe sobrepasar 3000", "OK");
+            }
+
+        }
+    }
+}
